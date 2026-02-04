b502c395cefff353f8bdbfacaae8eea4bec07e6d
[dyad] <git@dyad.sh>
2026-02-04 16:29:30 -0300

    [dyad] Corrigindo erro de imagem externa - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "a.storyblok.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+    ],
+  },
   webpack: (config) => {
     if (process.env.NODE_ENV === "development") {
       config.module.rules.push({
