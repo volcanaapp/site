8ff3ac382091d881d624a7a7ec4ee03504ca33e3
[dyad] <git@dyad.sh>
2026-02-04 16:23:13 -0300

    [dyad] Adicionando a seção 'Introducing Hero' - wrote 5 file(s)

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,6 +3,7 @@ import { getDictionary } from "@/lib/get-dictionary";
 import { Locale } from "@/lib/i18n-config";
 import { Hero02 } from "@/components/hero-02";
 import { PricingSection } from "@/components/pricing-section";
+import { Hero03 } from "@/components/hero-03";
 
 export default async function Home({
   params: { lang },
@@ -13,7 +14,8 @@ export default async function Home({
   return (
     <>
       <Hero01 dictionary={dictionary.hero} />
-      <Hero02 dictionary={dictionary} />
+      <Hero02 dictionary={dictionary.hero2} />
+      <Hero03 dictionary={dictionary.hero3} />
       <PricingSection dictionary={dictionary.pricing} />
     </>
   );
