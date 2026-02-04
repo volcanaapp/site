cc7dcce371457eb49610d5d33173cff3268b0672
[dyad] <git@dyad.sh>
2026-02-04 15:05:49 -0300

    [dyad] Removendo a seção de comparação de taxas - wrote 4 file(s), deleted 1 file(s)

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,4 @@
 import { Hero01 } from "@/components/hero-01";
-import { ComparisonGrid } from "@/components/comparison-grid";
 import { getDictionary } from "@/lib/get-dictionary";
 import { Locale } from "@/lib/i18n-config";
 import { Hero02 } from "@/components/hero-02";
@@ -13,7 +12,6 @@ export default async function Home({
   return (
     <>
       <Hero01 dictionary={dictionary.hero} />
-      <ComparisonGrid dictionary={dictionary.comparison} />
       <Hero02 dictionary={dictionary} />
     </>
   );
