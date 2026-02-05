f4bc1cb3db963f7900f04136fc80a0a1ad8ca161
[dyad] <git@dyad.sh>
2026-02-05 09:47:55 -0300

    [dyad] Restaurando a imagem correta do selo - wrote 2 file(s), deleted 1 file(s)

--- src/components/planet-badge.tsx
+++ src/components/planet-badge.tsx
@@ -14,7 +14,7 @@ export function PlanetBadge({ dictionary }: { dictionary: any }) {
         <HoverCardTrigger asChild>
           <div className="relative h-28 w-40 cursor-pointer overflow-hidden rounded-xl transition-transform duration-200 hover:scale-105">
             <Image
-              src="/volcana-nature.svg"
+              src="/volcana-nature.png"
               alt="Volcana Nature"
               fill
               sizes="160px"
