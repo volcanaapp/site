db60a02c774285f6169139b101e5670a770b04d6
[dyad] <volcanaapp@gmail.com>
2026-02-06 15:11:48 -0300

    [dyad] Tentando nova cena 3D - wrote 2 file(s)

---
+++ src/components/spline-animation.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Spline from '@splinetool/react-spline/next';
+
+export default function SplineAnimation() {
+  return (
+    <Spline
+      scene="https://my.spline.design/untitled-8sPGLwy4MxIBJMzD9L84K60v/" 
+      className="w-full h-full scale-150 lg:scale-125"
+    />
+  );
+}
