e69bd99e2c6a1253f06fd7abce18fabf9b4c45e5
[dyad] <volcanaapp@gmail.com>
2026-02-19 14:40:12 -0300

    [dyad] Adding social proof section - wrote 2 file(s)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { DesireSection } from "@/components/desire-section";
 import { WaitlistHeader } from "@/components/waitlist-header";
 import { HowItWorksSection } from "@/components/how-it-works-section";
 import { InterestSection } from "@/components/interest-section";
+import { SocialProofSection } from "@/components/social-proof-section";
 import { getDictionary } from "@/lib/get-dictionary";
 import { SimpleFooter } from "@/components/simple-footer";
 
@@ -17,6 +18,7 @@ export default async function WaitlistPage() {
       <WaitlistHeader />
       <main>
         <WaitlistHero dictionary={waitlistDict} />
+        <SocialProofSection title={waitlistDict.logos_title} />
         <HowItWorksSection />
         <InterestSection />
         <DesireSection />
