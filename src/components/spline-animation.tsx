6a968b715839c6afdf11870adf66287409b52d61
[dyad] <volcanaapp@gmail.com>
2026-02-20 10:47:35 -0300

    Reverted all changes back to version b733a9781809f63d466bf200ed6c0e22e030fc0b

--- src/components/spline-animation.tsx
+++ src/components/spline-animation.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import Spline from '@splinetool/react-spline/next';
-
 export default function SplineAnimation() {
   return (
-    <Spline
-      scene="https://prod.spline.design/8sPGLwy4MxIBJMzD9L84K60v/scene.splinecode" 
+    <iframe
+      src='https://my.spline.design/untitled-8sPGLwy4MxIBJMzD9L84K60v/'
+      frameBorder='0'
+      width='100%'
+      height='100%'
       className="w-full h-full scale-150 lg:scale-125"
-    />
+    ></iframe>
   );
 }
