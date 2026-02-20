91b6d13b6d4e5bc3fef054f6126ef57f524fd482
[dyad] <volcanaapp@gmail.com>
2026-02-20 13:51:26 -0300

    [dyad] Improving 3D animation loading - wrote 1 file(s)

--- src/components/spline-animation.tsx
+++ src/components/spline-animation.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Spline from '@splinetool/react-spline';
+
 interface SplineAnimationProps {
   sceneUrl?: string;
 }
@@ -8,12 +10,9 @@ const DEFAULT_URL = 'https://my.spline.design/untitled-8sPGLwy4MxIBJMzD9L84K60v/
 
 export default function SplineAnimation({ sceneUrl = DEFAULT_URL }: SplineAnimationProps) {
   return (
-    <iframe
-      src={sceneUrl}
-      frameBorder='0'
-      width='100%'
-      height='100%'
+    <Spline
+      scene={sceneUrl}
       className="w-full h-full scale-[1.5] lg:scale-[1.75]"
-    ></iframe>
+    />
   );
 }
