fcc7098e4b657a07207b29b4324d5a1255608ebb
[dyad] <volcanaapp@gmail.com>
2026-02-20 11:52:19 -0300

    [dyad] Fixing 3D animation overlap issue - wrote 1 file(s)

--- src/components/waitlist-header.tsx
+++ src/components/waitlist-header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "@/components/logo";
 
 export function WaitlistHeader() {
   return (
-    <header className="py-6">
+    <header className="py-6 relative z-20">
       <div className="container mx-auto flex justify-center">
         <Logo className="h-8" />
       </div>
