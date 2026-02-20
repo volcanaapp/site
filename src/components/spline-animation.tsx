6262c9b7b9123a6ec02e2d94248c263d89058386
[dyad] <volcanaapp@gmail.com>
2026-02-20 10:28:36 -0300

    [dyad] Atualizando a animação 3D - wrote 1 file(s)

--- src/components/spline-animation.tsx
+++ src/components/spline-animation.tsx
@@ -1,13 +1,12 @@
 "use client";
 
+import Spline from '@splinetool/react-spline/next';
+
 export default function SplineAnimation() {
   return (
-    <iframe
-      src='https://my.spline.design/untitled-8sPGLwy4MxIBJMzD9L84K60v/'
-      frameBorder='0'
-      width='100%'
-      height='100%'
+    <Spline
+      scene="https://prod.spline.design/6atEI76RYKKZm-Ig/scene.splinecode" 
       className="w-full h-full scale-150 lg:scale-125"
-    ></iframe>
+    />
   );
 }
