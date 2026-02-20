147b15c8827061edd22ff6a462aab282d94e934e
[dyad] <volcanaapp@gmail.com>
2026-02-20 10:46:18 -0300

    [dyad] Simplificando a animação 3D - wrote 1 file(s)

--- src/components/spline-animation.tsx
+++ src/components/spline-animation.tsx
@@ -5,7 +5,7 @@ import Spline from '@splinetool/react-spline/next';
 export default function SplineAnimation() {
   return (
     <Spline
-      scene="https://prod.spline.design/dMwNhkcMhsIWW8PB/scene.splinecode" 
+      scene="https://prod.spline.design/8sPGLwy4MxIBJMzD9L84K60v/scene.splinecode" 
       className="w-full h-full scale-150 lg:scale-125"
     />
   );
